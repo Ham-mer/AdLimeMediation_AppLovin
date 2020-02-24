cb2d808a27088b02c89ef06d099d4fdf183f6264
Ham-mer <2419568879@qq.com>
2020-02-24 09:56:20 +0800

    .

---
+++ AdLimeMediation_AppLovin.framework/Headers/AdLimeAppLovinGlobalConfig.h
@@ -0,0 +1,22 @@
+//
+//  AdLimeAppLovinGlobalConfig.h
+//  AdLimeMediation_AppLovin
+//
+//  Created by AdLimeSdk on 2020/2/23.
+//  Copyright © 2020 AdLimeSdk. All rights reserved.
+//
+
+#import <AdLimeSdk/AdLimeSdk.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AdLimeAppLovinGlobalConfig : AdLimeNetworkConfig
+
+/**
+ * Determines whether to begin video ads in a muted state or not. Defaults to NO unless changed in the dashboard.
+ */
+@property (assign, atomic) BOOL muted;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AdLimeMediation_AppLovin.framework/Headers/AdLimeMediation_AppLovin.h
@@ -0,0 +1,19 @@
+//
+//  AdLimeMediation_AppLovin.h
+//  AdLimeMediation_AppLovin
+//
+//  Created by AdLimeSdk on 2018/9/28.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdLimeMediation_AppLovin.
+FOUNDATION_EXPORT double AdLimeMediation_AppLovinVersionNumber;
+
+//! Project version string for AdLimeMediation_AppLovin.
+FOUNDATION_EXPORT const unsigned char AdLimeMediation_AppLovinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdLimeMediation_AppLovin/PublicHeader.h>
+
+#import <AdLimeMediation_AppLovin/AdLimeAppLovinGlobalConfig.h>
+#import <AdLimeMediation_AppLovin/AdLimeAppLovinNativeConfig.h>
